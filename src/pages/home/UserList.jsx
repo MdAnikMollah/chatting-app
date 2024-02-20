7b16b45b1af9af00353dce1c8989acd060cf45b3
Mohammad Anik Mollah <mdanik0178@gmail.com>
2024-02-20 22:41:07 +0600

    reciever

--- src/pages/home/UserList.jsx
+++ src/pages/home/UserList.jsx
@@ -1,111 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import GroupCard from '../../components/home/GroupCard'
 import { FaPlus } from 'react-icons/fa'
-import Image from '../../utils/Image'
-
+import Image from '../../utils/image';
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
+import { useSelector, useDispatch } from 'react-redux'
 const UserList = () => {
+  const [userList,setUserList] = useState()
+  const db = getDatabase();
+  const data = useSelector((state) => state.loginuserdata.value)
+  console.log(data);
+useEffect(()=>{
+  const userRef = ref(db, 'users');
+  onValue(userRef, (snapshot) => {
+  let arr = []
+  snapshot.forEach((item)=>{
+    if(data.uid != item.key){
+      arr.push({...item.val(),id:item.key});
+
+    }
+ })
+ setUserList(arr)
+});
+
+},[])
+let handleFRequest = (frequestinfo) => {
+console.log(frequestinfo);
+set(push(ref(db,"friendrequest")),{
+  senderid: data.uid,
+  sendername: data.displayName,
+  senderimg: data.photoURL,
+  senderemail: data.email,
+  receiverid: frequestinfo.id,
+  receivername: frequestinfo.username,
+  receiveremail: frequestinfo.email,
+  receiverimg: frequestinfo.profileImg
+})
+}
+
   return (
    <>
    <GroupCard cardtitle="User List"> 
         <div className='usermainbox'>
-          <div className='useritem'>
-            <div className="userimgbox">
-              <Image src="" alt="img"/>
-            </div>
-            <div className='userinfobox'>
-              <div>
-                <h3>Anik</h3>
-                <p>MERN developer</p>
-              </div>
-              <button className='addbutton'>
-                <FaPlus />
-              </button>
-            </div>
+        {userList && userList.length > 0
+        ?
+        userList.map((item,index)=>
+          
+        <div key={index} className='useritem'>
+          <div className="userimgbox">
+            <Image src={item.profileImg} alt="img"/>
           </div>
-          <div className='useritem'>
-            <div className="userimgbox">
-              <Image src="" alt="img"/>
+          <div className='userinfobox'>
+            <div>
+              <h3>{item.username}</h3>
+              <p>MERN developer</p>
             </div>
-            <div className='userinfobox'>
-              <div>
-                <h3>Anik</h3>
-                <p>MERN developer</p>
-              </div>
-              <button className='addbutton'>
-                <FaPlus />
-              </button>
-            </div>
+            <button onClick={()=>handleFRequest(item)} className='addbutton'>
+              <FaPlus />
+            </button>
           </div>
-          <div className='useritem'>
-            <div className="userimgbox">
-              <Image src="" alt="img"/>
-            </div>
-            <div className='userinfobox'>
-              <div>
-                <h3>Anik</h3>
-                <p>MERN developer</p>
-              </div>
-              <button className='addbutton'>
-                <FaPlus />
-              </button>
-            </div>
-          </div>
-          <div className='useritem'>
-            <div className="userimgbox">
-              <Image src="" alt="img"/>
-            </div>
-            <div className='userinfobox'>
-              <div>
-                <h3>Anik</h3>
-                <p>MERN developer</p>
-              </div>
-              <button className='addbutton'>
-                <FaPlus />
-              </button>
-            </div>
-          </div>
-          <div className='useritem'>
-            <div className="userimgbox">
-              <Image src="" alt="img"/>
-            </div>
-            <div className='userinfobox'>
-              <div>
-                <h3>Anik</h3>
-                <p>MERN developer</p>
-              </div>
-              <button className='addbutton'>
-                <FaPlus />
-              </button>
-            </div>
-          </div>
-          <div className='useritem'>
-            <div className="userimgbox">
-              <Image src="" alt="img"/>
-            </div>
-            <div className='userinfobox'>
-              <div>
-                <h3>Anik</h3>
-                <p>MERN developer</p>
-              </div>
-              <button className='addbutton'>
-                <FaPlus />
-              </button>
-            </div>
-          </div>
-          <div className='useritem'>
-            <div className="userimgbox">
-              <Image src="" alt="img"/>
-            </div>
-            <div className='userinfobox'>
-              <div>
-                <h3>Anik</h3>
-                <p>MERN developer</p>
-              </div>
-              <button className='addbutton'>
-                <FaPlus />
-              </button>
-            </div>
-          </div>
+        </div>
+        )
+        :
+        <h2>No user available</h2>
+        }
+          
         </div>
       </GroupCard>
    </>
